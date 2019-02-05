04c9d54208fd2d65ee814d4131b73c8ab784ec9b
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2019-02-05 17:40:02 +0300

    Add underdeveloped example for dynamic memory and classes

---
+++ C++ Primer Plus Practice/Classes/DynamicClasses101/StringBad.cpp
@@ -0,0 +1,60 @@
+//
+//  StringBad.cpp
+//  C++ Primer Plus Practice
+//
+//  Created by Anthony on 05/02/2019.
+//  Copyright © 2019 Anthony. All rights reserved.
+//
+//  Flawed (underdeveloped) string class
+//
+
+#include <cstring>
+#include "StringBad.hpp"
+
+namespace DynamicClasses101
+{
+    // Initialise static class member
+    int StringBad::strCount = 0;
+    
+    // Class methods
+    StringBad::StringBad(const char* s)                 // Construct from a C-string
+    {
+        length = static_cast<int>(std::strlen(s));      // set string size
+        str = new char[length + 1];                     // allot storage
+        std::strcpy(str, s);                            // initialise allocated storage
+        strCount++;                                     // update object count
+        
+    #ifdef VERBOSE_STRING_
+        std::cout << strCount << ": \"" << str << "\" object created\n";
+    #endif
+    }
+    
+    StringBad::StringBad()                              // Default constructor
+    {
+        length = 4;
+        str = new char[4];
+        std::strcpy(str, "C++");                        // default string
+        strCount++;
+        
+    #ifdef VERBOSE_STRING_
+        std::cout << strCount << ": \"" << str << "\" default object created\n";
+    #endif
+    }
+    
+    StringBad::~StringBad()                             // Necessary destructor
+    {
+    #ifdef VERBOSE_STRING_
+        std::cout << "\"" << str << "\" object deleted, "
+                  << strCount - 1 << " strings left\n";
+    #endif
+        
+        strCount--;                                     // required
+        delete [] str;                                  // required
+    }
+    
+    std::ostream& operator<<(std::ostream& os, const StringBad& s)
+    {
+        os << s.str;
+        return os;
+    }
+}
---
+++ C++ Primer Plus Practice/Classes/DynamicClasses101/StringBad.hpp
@@ -0,0 +1,35 @@
+//
+//  StringBad.hpp
+//  C++ Primer Plus Practice
+//
+//  Created by Anthony on 05/02/2019.
+//  Copyright © 2019 Anthony. All rights reserved.
+//
+//  Flawed (underdeveloped) string class
+//
+
+#ifndef StringBad_hpp
+#define StringBad_hpp
+
+#include <iostream>
+
+namespace DynamicClasses101
+{
+    class StringBad
+    {
+    private:
+        char* str;                  // pointer to string
+        int length;                  // length of string
+        static int strCount;        // number of objects
+        
+    public:
+        StringBad(const char* s);   // constructor
+        StringBad();                // default constructor
+        ~StringBad();               // destructor
+        
+//      I/O Operator overloads
+        friend std::ostream& operator<<(std::ostream& os, const StringBad& st);
+    };
+}   // end namespace DynamicClasses101
+
+#endif /* StringBad_hpp */
