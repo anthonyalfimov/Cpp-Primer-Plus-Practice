822000fb7688c615205ee61cf5e5f904a6a77168
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2019-02-11 15:59:18 +0300

    Improve verbose destructor output
    Add a console output line after memory deallocation command to indicate
    whether destructor execution successfully proceeded past `delete []`

--- C++ Primer Plus Practice/Classes/DynamicClasses101/StringBad01.cpp
+++ C++ Primer Plus Practice/Classes/DynamicClasses101/StringBad01.cpp
@@ -45,12 +45,16 @@ namespace DynamicClasses101
     StringBad::~StringBad()                             // Necessary destructor
     {
     #ifdef VERBOSE_STRINGBAD_
-        std::cout << "\"" << str << "\" object deleted, "
-                  << strCount - 1 << " strings left\n";
+        std::cout << "\"" << str << "\" object being deleted, "
+                  << strCount - 1 << " strings left... ";
     #endif
         
         strCount--;                                     // required
         delete [] str;                                  // required
+        
+    #ifdef VERBOSE_STRINGBAD_
+        std::cout << "Done.\n";
+    #endif
     }
     
     std::ostream& operator<<(std::ostream& os, const StringBad& s)
